1ee5abc92575b98b7dfdd4a583b263204a0197e0
early_necromancer <gideon@docurel.net>
2026-09-22 12:43:21 +0300

    turret functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fataopmode/impl/robot/turret/TurretSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fataopmode/impl/robot/turret/TurretSubsystem.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.fataopmode.impl.robot.turret;
 
+import static org.firstinspires.ftc.teamcode.fataopmode.impl.robot.drive.DriveSubsystem.drive;
+
+import com.pedropathing.math.Pose;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.hardware.servos.ServoEx;
 import com.seattlesolvers.solverslib.hardware.servos.ServoExGroup;
 
 import org.firstinspires.ftc.teamcode.fataopmode.api.robot.hardware.Subsystem;
+
+import java.util.function.Supplier;
 
 public class TurretSubsystem extends Subsystem {
     private static final TurretSubsystem turret = new TurretSubsystem();
@@ -12,6 +17,8 @@ public class TurretSubsystem extends Subsystem {
     private ServoEx servoOne;
     private ServoEx servoTwo;
     private ServoExGroup turretServos;
+
+    private Supplier<Double> target = () -> 0.0;
 
     public static TurretSubsystem turret(){
         return turret;
@@ -47,4 +54,27 @@ public class TurretSubsystem extends Subsystem {
     public double getTurretAngle(){
         return turretServos.get();
     }
+
+    private double getDegreesTo(Pose target){
+         double angle = Math.toDegrees(
+                 Math.atan2(
+                         target.y() - drive().getY(),
+                         target.x() - drive().getX()
+                 )
+         );
+
+        if (angle < 0)
+            angle += 360;
+
+        return angle;
+    }
+
+    public void setTarget(Supplier<Double> target){
+        this.target = target;
+    }
+
+    public double getTarget(){
+        return target.get();
+    }
+
 }
